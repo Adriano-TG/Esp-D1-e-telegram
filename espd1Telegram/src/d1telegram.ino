36b30947bb486fbc99a28ee0d8969afe1e94e299
Adriano Tavares <adrianotavares32@gmail.com>
2020-03-30 17:28:07 -0300

    modifica  arquivo d1

--- espd1Telegram/src/d1telegram.ino
+++ espd1Telegram/src/d1telegram.ino
@@ -16,7 +16,7 @@ void setup() {
    Serial.begin(115200);
    lcd.print("TelegramBot...");
    myBot.wifiConnect("Net","44643904"); 
-   myBot.setTelegramToken("1084311211:AAGyuARTxFoe-Dvy2FEmuJ8a5vjygL90Gbw"); 
+   myBot.setTelegramToken("1084311211:xxxxxxxxxxxxxxxxxxxxxxxxxxxx"); 
    
    myKbd.addButton("Ligar",on,CTBotKeyboardButtonQuery);
    myKbd.addButton("Desligar",off,CTBotKeyboardButtonQuery);
